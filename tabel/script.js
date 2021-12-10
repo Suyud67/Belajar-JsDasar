d726543b02b91c9567132dcf8eb19e84b9ad0c12
Suyud Setiawan Al Arif <77600520+Suyud67@users.noreply.github.com>
2021-12-10 08:57:30 +0800

    upload file belajar-JsDasar

---
+++ tabel/script.js
@@ -0,0 +1,11 @@
+var jmlAngkot = 10;
+var angkotBeroprasi = 6;
+var noAngkot = 1;
+while(noAngkot <= angkotBeroprasi){
+	console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.' );
+noAngkot++;
+}
+for (noAngkot = angkotBeroprasi +1;noAngkot <= jmlAngkot; noAngkot++) {
+	console.log('Angkot no. ' + noAngkot + ' tidak beroprasi dengan baik.');
+	
+}
